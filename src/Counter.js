e81ec08fea93a39761609fbbcf97214f953f263e
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-19 11:34:14 +0300

    Crazy

--- src/Counter.js
+++ src/Counter.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 function Counter() {
     const [num, setNum] = useState(0);
+    const [multiplier, setMultiplier] = useState(1);
     function addOne() {
         console.log("Funkcija izsaukta");
         setNum(num+1);
@@ -10,11 +11,20 @@ function Counter() {
     function minusOne() {
         setNum(num-1);
     }
+    function multiply(){
+        setNum(num * multiplier);
+    }
+    function handleChange(e){
+        setMultiplier(e.target.value);
+    }
+
 
     return (
     <div>
         <button onClick={addOne}>+1</button>
         <button onClick={minusOne}>-1</button>
+        <input type="number" value={multiplier} onChange={handleChange} />
+        <button onClick={multiply}>Reizinat</button>
         <h1>{num}</h1>
     </div>
 
